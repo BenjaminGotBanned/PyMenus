977daa2fcafbb077beab21f7d55e16db72a123c6
Benjamin O'Brien <bobrien9274@gmail.com>
2020-06-26 16:32:33 -0500

    V1.0.4 (PyPi)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "PyMenus",
-    version = "1.0.3",
+    version = "1.0.4",
     author = "Benjamin O'Brien",
     author_email = "bobrien9274@gmail.com",
     description = "Simple, terminal-based menus for Python",
